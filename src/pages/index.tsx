62e3731dab5af09140264ac6f87cc750613eb2ad
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-04-11 19:15:51 +0530

    Added navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+
 export default function Home() {
   return (
     <main>
-      Working
+      <nav className="bg-cyan-800 py-4 px-8 flex justify-between">
+        <h1 className='text-2xl font-bold'>John Doe</h1>
+        <ul className='flex items-center gap-4'>
+          <li>
+            <BsFillMoonStarsFill className='cursor-pointer' />
+          </li>
+          <li><a href="#" className='bg-white text-cyan-900 px-4 py-2 rounded'>Resume</a></li>
+        </ul>
+      </nav>
     </main>
   )
 }
